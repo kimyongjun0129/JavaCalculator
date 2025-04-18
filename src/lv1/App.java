dd795f664d852fc002bcc7e7b833041bff324768
kimyongjun0129 <kimyongjun0129@naver.com>
2025-04-18 10:50:27 +0900

    main -> app으로 이름 변경

--- src/lv1/App.java
+++ src/lv1/App.java
@@ -3,7 +3,7 @@ package lv1;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Calculator01 {
+public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
--- src/lv2/App.java
+++ src/lv2/App.java
@@ -3,7 +3,7 @@ package lv2;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Calculator02 {
+public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Calculator calculator = new Calculator();
